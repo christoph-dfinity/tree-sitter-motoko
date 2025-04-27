0c55fb76e96b1061e3bb579e6e715d057ae04ee9
Christoph Hegemann <christoph.hegemann@dfinity.org>
2025-04-27 12:40:33 +0200

    types

--- grammar.js
+++ grammar.js
@@ -15,6 +15,10 @@ function semi_sep(rule) {
   return seq(repeat(seq(rule, ";")), optional(rule));
 }
 
+function semi_sep1(rule) {
+  return seq(repeat(seq(rule, ";")), rule, optional(";"));
+}
+
 module.exports = grammar({
   name: "motoko",
   extras: $ => [/\s+/, $.doc_comment, $.line_comment, $.block_comment],
@@ -31,6 +35,7 @@ module.exports = grammar({
 
     // Identifiers
     identifier: $ => /[a-zA-Z_][a-zA-Z_0-9]*/,
+    _type_identifier: $ => alias($.identifier, $.type_identifier),
     privileged_identifier: $ => seq("@", token.immediate($.identifier)),
 
     // Literals
@@ -53,7 +58,15 @@ module.exports = grammar({
     ),
 
     // Misc
-    typ_annot: $ => seq(":", $._type),
+    typ_annot: $ => prec.right(seq(":", $._typ)),
+    typ_path: $ => seq(
+      repeat(seq($.identifier, ".")),
+      $._type_identifier,
+    ),
+    path: $ => seq(
+      repeat(seq($.identifier, ".")),
+      $.identifier,
+    ),
 
     // Declarations
     _dec: $ => choice(
@@ -61,6 +74,7 @@ module.exports = grammar({
       $.var_dec,
       $.let_dec,
       $.let_else_dec,
+      $.typ_dec,
     ),
 
     exp_dec: $ => $._exp,
@@ -82,6 +96,13 @@ module.exports = grammar({
       $.let_dec,
       "else",
       $._exp
+    ),
+    typ_dec: $ => seq(
+      "type",
+      $._type_identifier,
+      optional($.typ_params),
+      "=",
+      $._typ,
     ),
 
     // Expressions
@@ -107,10 +128,136 @@ module.exports = grammar({
     )),
 
     // Types
-    _type: $ => choice(
-      $.var_ty,
+    _typ_nullary: $ => choice(
+      $.tup_typ,
+      $.path_typ,
+      $.array_typ,
+      $.obj_typ,
+      $.variant_typ,
     ),
-    var_ty: $ => $.identifier,
+    _typ_un: $ => choice(
+      $._typ_nullary,
+      $.quest_typ,
+    ),
+    _typ_pre: $ => choice(
+      $._typ_un,
+      $.prim_typ,
+      $.async_typ,
+      $.obj_sort_typ,
+    ),
+    _typ_no_bin: $ => choice(
+      $._typ_pre,
+      $.func_typ,
+    ),
+    _typ: $ => choice(
+      $._typ_no_bin,
+      $.and_typ,
+      $.or_typ,
+    ),
+    and_typ: $ => prec.left(
+      seq($._typ, "and", $._typ),
+    ),
+    or_typ: $ => prec.left(
+      seq($._typ, "or", $._typ),
+    ),
+    tup_typ: $ => seq(
+      "(",
+      comma_sep($.typ_item),
+      ")"
+    ),
+    path_typ: $ => seq(
+      $.typ_path,
+      optional(seq(
+        "<",
+        comma_sep($._typ),
+        ">",
+      )),
+    ),
+    array_typ: $ => seq(
+      "[",
+      optional("var"),
+      $._typ,
+      "]",
+    ),
+    obj_typ: $ => seq(
+      "{",
+      semi_sep($._typ_field),
+      "}",
+    ),
+    variant_typ: $ => choice(
+      seq("{", "#", "}"),
+      seq(
+        "{",
+        semi_sep1($.typ_tag),
+        "}",
+      ),
+    ),
+    quest_typ: $ => seq("?", $._typ_un),
+    prim_typ: $ => seq("prim", $.text_literal),
+    async_typ: $ => seq(
+      choice("async", "async*"),
+      $._typ_pre,
+    ),
+    obj_sort_typ: $ => seq(
+      choice("module", "actor", "object"),
+      $.obj_typ,
+    ),
+    func_typ: $ => seq(
+      optional("shared"),
+      optional("composite"),
+      optional("query"),
+      optional($.typ_params),
+      $._typ_un,
+      "->",
+      $._typ_no_bin,
+    ),
+    typ_item: $ => seq(
+      optional(seq($.identifier, ':')),
+      $._typ,
+    ),
+    typ_tag: $ => seq(
+      "#",
+      $.identifier,
+      optional($.typ_annot),
+    ),
+    _typ_field: $ => choice(
+      $.typ_tf,
+      $.val_tf,
+      $.func_tf,
+    ),
+    typ_tf: $ => seq(
+      "type",
+      $._type_identifier,
+      optional($.typ_params),
+      "=",
+      $._typ,
+    ),
+    val_tf: $ => seq(
+      optional("var"),
+      $.identifier,
+      ":",
+      $._typ,
+    ),
+    func_tf: $ => seq(
+      $.identifier,
+      optional($.typ_params),
+      $._typ_nullary,
+      "=",
+      $._typ,
+    ),
+    typ_params: $ => seq(
+      "<",
+      optional("system"),
+      comma_sep($.typ_bind),
+      ">",
+    ),
+    typ_bind: $ => seq(
+      $._type_identifier,
+      optional(seq(
+        "<:",
+        $._typ,
+      )),
+    ),
 
     // Patterns
     _pat: $ => $._pat_bin,
